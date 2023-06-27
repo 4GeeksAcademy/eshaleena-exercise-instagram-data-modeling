0d1bf33d229987e71af9ae54b1a0ab9a790dd30d
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-27 20:23:35 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), unique = True, nullable=False)
+    firstname = Column(String(250), unique = True, nullable=False)
+    lastname = Column(String(250), unique = True, nullable=False)
+    email = Column(String(250), unique = True, nullable=False)
+    password = Column(String(250), nullable = False)
+
+
+class Media(Base):
+    __tablename__ = 'chararcter'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), unique = True, nullable = False)
+    birth_year = Column(Integer, nullable = False)
+    homeworld = Column(String(250), nullable = False)
+    person_id = Column(Integer, ForeignKey('character.id'))
+    person = relationship(Chararcter)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), unique = True, nullable=False)
+    password = Column(String(250), nullable = False)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), unique = True, nullable=False)
+    password = Column(String(250), nullable = False)
+
 
     def to_dict(self):
         return {}
